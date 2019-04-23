3b0317b6218dbe6e36f89032081656686954e60e
toannguyen0592 <36652669+toannguyen0592@users.noreply.github.com>
2019-04-23 11:26:43 -0400

    Update submit.php

--- submit.php
+++ submit.php
@@ -83,8 +83,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous"> -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-        <link rel="stylesheet" href="/~tn04662/sc/css/style.css">
-        <link rel="stylesheet" href="/~tn04662/sc/css/submit.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/submit.css">
         <style>
         
         </style>
