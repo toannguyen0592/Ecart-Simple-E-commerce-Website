2798c3e7e18433ec08123142e00adfa7d716e985
toannguyen0592 <36652669+toannguyen0592@users.noreply.github.com>
2019-04-23 11:27:55 -0400

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -14,8 +14,8 @@
         <!-- Bootstrap CSS -->
         <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous"> -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-        <link rel="stylesheet" href="/~tn04662/sc/css/style.css" type="text/css" />
-        <link rel="stylesheet" href="/~tn04662/sc/css/checkout.css" type="text/css" />
+        <link rel="stylesheet" href="css/style.css" type="text/css" />
+        <link rel="stylesheet" href="css/checkout.css" type="text/css" />
         <style>
         
         </style>
