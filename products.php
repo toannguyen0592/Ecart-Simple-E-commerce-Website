ddff1c5c9dddb8aa1699b93c4984a65b6ec57264
toannguyen0592 <36652669+toannguyen0592@users.noreply.github.com>
2019-04-23 11:25:46 -0400

    Update products.php

--- products.php
+++ products.php
@@ -60,8 +60,8 @@ if (!empty($_GET["action"])) {
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
             integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
         <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"> -->
-        <link rel="stylesheet" href="/~tn04662/sc/css/style.css" type="text/css">
-        <link rel="stylesheet" href="/~tn04662/sc/css/products.css" type="text/css">
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+        <link rel="stylesheet" href="css/products.css" type="text/css">
         <!-- Internal CSS -->
         <style>
         
